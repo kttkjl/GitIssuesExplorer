c08b40b15e64bc1d3bbf8d4aa24c947d1b1ef5e1
EC2 Default User <ec2-user@ip-172-31-27-78.us-east-2.compute.internal>
2019-06-22 23:49:27 +0000

    changed homepage

--- server_githubIssue.js
+++ server_githubIssue.js
@@ -7,11 +7,13 @@ const port = 3001;
 app.use(express.static(path.join(__dirname, "build")));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/GithubIssueExplorer", (req, res) => {
+app.get("*", (req, res) => {
+  console.log("githubissexp hit");
   res.sendFile(path.join(__dirname, "build", "index.html"));
   console.log(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => {
+  console.log(path.join(__dirname, "build", "index.html"));
   console.log(`GithubIssueExplorer app listening on port ${port}!`);
 });
